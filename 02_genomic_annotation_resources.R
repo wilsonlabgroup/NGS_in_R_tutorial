52dcb85a71236c9615a8156209020331a2fd8b0a
hy09 <huayunhou@gmail.com>
2021-11-15 22:30:17 -0500

    added EnsDb content; modified BSgenomes example

--- 02_genomic_annotation_resources.R
+++ 02_genomic_annotation_resources.R
@@ -1,19 +1,22 @@
 ####################################################################
 # CCM bioinfo tutorial -- NGS analysis in R
-# 2020.11.17
+# 2021.11.15
 # Part 2: Genomic annotation resources in R
 # Author: Huayun Hou (huayun.hou@sickkids.ca)
 # Resource: 
 #   https://www.bioconductor.org/packages/release/workflows/vignettes/annotation/inst/doc/Annotation_Resources.html
+# BioC workshop: http://biocworkshops2019.bioconductor.org.s3-website-us-east-1.amazonaws.com/page/Bioc2019Anno__AnnotationWorkshop/
 ####################################################################
 
 
 # AnnotationDbi: orgDb and TxDb -------------------------------------------
 
+# core functions: columns(), keytypes(), keys(), select()
+
 # OrgDb: Organism level, gene centric, stores mapping between identifiers and other information.
 library(org.Hs.eg.db)
 
-# Methods for AnnotationDb object
+# Methods for AnnotationDbi object
 columns(org.Hs.eg.db)
 keytypes(org.Hs.eg.db)
 head(keys(org.Hs.eg.db)) # entrez id 
@@ -32,22 +35,31 @@ ensembl_to_symbol <- mapIds(org.Hs.eg.db,
        keytype = "ENSEMBL",
        column = "SYMBOL")
 
-# TxDb: transcript oriented, maps between genes and transcripts, and their genomic intervals
+# when there are multiple values to be returned? Use multiVals to determine behavior
+ensembl_to_symbol_multi <- mapIds(org.Hs.eg.db, 
+                            keys = some_genes,
+                            keytype = "ENSEMBL",
+                            column = "GO",
+                            multiVals = "list")
+
+# TxDb and EnsDb: transcript oriented, maps between genes and transcripts, and their genomic intervals
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 
-# Exercise: use columns() and keytypes() to explore the txdb object
-#           use select to obtain the transcript name and genomic locations of a few genes
-
+# use columns() and keytypes() to explore the txdb object
+# use select to obtain the transcript name and genomic locations of a few genes
 
 # use select() txdb
-some_genes <- keys(txdb, keytype = "GENEID")[100:105]
+set.seed(1234)
+some_genes <- keys(txdb, keytype = "GENEID")[sample(1:23000, 10)]
 select(txdb, 
        keys = some_genes,
        keytype = "GENEID",
        columns = c("GENEID","TXNAME","TXCHROM","TXSTART","TXEND","TXSTRAND"))
 
 # txdb specific methods using the GenomicFeatures package
+# https://bioconductor.org/packages/devel/bioc/vignettes/GenomicFeatures/inst/doc/GenomicFeatures.pdf
+# TxDb objects can be made from gtf/gff files 
 
 transcripts(txdb)[1:10]
 exonsBy(txdb, by = "gene")[1:2]
@@ -62,90 +74,46 @@ fiveUTRsByTranscript(txdb)[1:10]
 threeUTRsByTranscript(txdb)
 intronsByTranscript(txdb)
 
-# Given some genes, find their promoter regions 
-some_genes <- keys(org.Hs.eg.db, keytype = "SYMBOL")[1:10]
+# EnsDb is similar to Txdb, but with ensembl-oriented annotations and additional functions
+# Learn more at: https://bioconductor.org/packages/release/bioc/html/ensembldb.html
 
-gene_entrez <- select(org.Hs.eg.db, keys = some_genes, keytype = "SYMBOL", column = "ENTREZID")
+library(EnsDb.Hsapiens.v75)
+edb <- EnsDb.Hsapiens.v75
 
-gene_prom <- promoters(txdb, 
-          upstream = 3000, 
-          downstream = 3000, 
-          filter = list(gene_id = gene_entrez$ENTREZID),
-          columns = c("tx_name","gene_id"))
+edb
 
+# With AnnotationDbi methods
+columns(edb)
+keytypes(edb)
 
+some_genes <- keys(edb, keytype = "SYMBOL")[sample(1:20000, 10)]
 
-# match between gene entrez id and symbol
-temp <- gene_entrez$SYMBOL
-names(temp) <- gene_entrez$ENTREZID
+gene_info <- select(edb, 
+                    keytype = "SYMBOL",
+                    keys = some_genes, 
+                    columns = c("SYMBOL","GENEID","TXBIOTYPE","SEQNAME", "GENESEQSTART","GENESEQEND"))
 
-# add to Grange object 
-gene_prom$gene_symbol <- temp[unlist(gene_prom$gene_id)]
+# EnsDb specific:
+# Filters
+supportedFilters(edb)
 
-# Making TxDb object from gtf/gff files and more about TxDb objects, see:
-# https://bioconductor.org/packages/release/bioc/vignettes/GenomicFeatures/inst/doc/GenomicFeatures.pdf
+# Filter with a "filter" object
+tx <- transcripts(edb, filter = GeneNameFilter("TP53"))
 
-# In addition, EnsDb packages provides ensembl centric genes annotations (gene name, id, transcripts, location, biotype etc.) and can be access using the same select() method. 
-# https://bioconductor.org/packages/release/bioc/html/ensembldb.html
+# Filter with a formula: ~ <field> <condition> <value>
+tx <- transcripts(edb, filter = ~gene_name == "TP53")
 
-# organism.dplyr ----------------------------------------------------------
-# Since OrgDb and TxDb contain different information, it is bothersome to match between the two packages. Organism.dplyr makes a sqlite database of a organism from its corresponding orgDb and TxDb, queries both databases and merges the results, making it easier to get all information from "one resource". 
+tx_pc <- transcripts(edb, filter = (~gene_name == "TP53" & tx_biotype == "protein_coding"))
 
-library(Organism.dplyr)
+# Specify columns to return from extractor methods ("genes","transcripts", "exons")
 
-# see supported organisms that have both OrgDb and TxDb:
-supportedOrganisms()
-
-# initiate a src_organism object
-src <- src_organism("TxDb.Hsapiens.UCSC.hg19.knownGene")
-
-columns(src)
-
-# Methods 
-
-# 1.similar as in OrgDb and TxDb, select() can be used to retrieve information from an src_organism object
-
-select(src, 
-       keys = "TP53",
-       keytype = "symbol",
-       columns = c("alias","tx_name", "tx_start"))
-
-# 2. methods from TxDb packages can be used with additional filters
-supportedFilters(src)
-
-# create filters
-gr_filter <- GRangesFilter(GRanges("chr1:30000000-40000000"))
-symbol_filter <- SymbolFilter("A", "startsWith")
-
-promoters(src, 
-          filter = ~gr_filter & symbol_filter)
-
-transcripts(src, 
-            filter = ~symbol %startsWith% "A" & gr_filter)
-
-# Example:get promoter sequences for all transcripts on chr17
-
-# tx_chrom filter does not work on "promoters()" directly, thus we get transcripts first, and use the promoters() function to get promoters. 
-chr17_promoters <- promoters(transcripts(src,
-                             filter = ~tx_chrom == "chr17",
-                             columns = c("symbol")),
-                            upstream = 3000,
-                            downstream = 3000)
-
-# 3. dplyr methods 
-# count the number of transcripts for each gene on chr22
-# see vignette for more information
-
-tbl(src, "ranges_tx") %>% 
-        filter(tx_chrom == "chr17" & !is.na(entrez)) %>% 
-        left_join(tbl(src, "id"), by = "entrez") %>% 
-        dplyr::select(symbol,ensembl, tx_name) %>% 
-        distinct() %>% 
-        group_by(symbol, ensembl) %>% 
-        summarise(nTranscripts = n()) %>% 
-        arrange(desc(nTranscripts))
+# get all promoter regions on chr17
+chr17_promoters <- promoters(transcripts(edb, 
+                                         filter = ~seq_name == 17 & tx_biotype == "protein_coding",
+                                         columns = c("seq_name","tx_seq_start", "tx_seq_end", "tx_name","symbol","protein_id")))
 
 
+# Other useful functions include converting between genome, transcript, and protein coordinate 
 
 # BSgenomes ---------------------------------------------------------------
 # BSgenomes packages contain sequence data for a specific genome build of an organism
@@ -153,32 +121,33 @@ library(BSgenome)
 available.genomes()
 installed.genomes()
 
-BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
-library("BSgenome.Hsapiens.UCSC.hg19")
+# install a BSgenome
+BiocManager::install("BSgenome.Celegans.UCSC.ce11")
+library("BSgenome.Celegans.UCSC.ce11")
 
-Hsapiens
+Celegans
 
 # see all chromosomes
-seqnames(Hsapiens)
+seqnames(Celegans)
 
-# access sequence of one chromosome
-chr17_seq <- Hsapiens[["chr17"]]
-
-# convert to a XStringSet to save as a fasta file (we can only write XstringSet, not Xstring)
-chr17_seq <- DNAStringSet(chr17_seq)
-names(chr17_seq) <- "chr17"
+# access sequence of one chromosome, the result is a DNAString object
+chrI_seq <- Celegans[["chrI"]]
 
 # write chr17 sequence into a gzipped fasta file
-writeXStringSet(chr17_seq, "hg19_chr17.fasta.gz", format = "fasta", compress = T)
+# convert to a XStringSet to save as a fasta file (we can only write XstringSet, not Xstring)
+chrI_seq <- DNAStringSet(chrI_seq)
+names(chrI_seq) <- "chrI"
 
+# output
+writeXStringSet(chrI_seq, "ce11_chrI.fasta.gz", format = "fasta", compress = T)
 
 # we can use Biostring functions on the sequence
 # calculate GC content
-letterFrequency(chr17_seq, "GC", as.prob = T)
+letterFrequency(chrI_seq, "GC", as.prob = T)
 
-
-# get promoter sequences
-getSeq(Hsapiens, chr17_promoters)
+# get sequences
+example_region <- GRanges(seqnames = "chrI", IRanges(100,200), strand = "-")
+getSeq(Celegans, example_region)
 
 # AnnotationHub -----------------------------------------------------------
 # A client interface to abundant resources stored at the AnnotationHub web service 
@@ -186,6 +155,8 @@ getSeq(Hsapiens, chr17_promoters)
 library(AnnotationHub)
 # initiate an AnnotationHub instance
 ah <- AnnotationHub()
+removeCache(ah)
+ah
 
 # use [] to get information about a specific dataset
 ah[1000]
